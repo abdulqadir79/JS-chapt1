d88e27f7814613a9c30dfdd9d4a035424dc5044a
Abdul Qadir <87005175+abdulqadir79@users.noreply.github.com>
2024-02-26 13:20:57 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+//1. Write a script to greet your website visitor using JS alert
+//box.
+//alert("Hello World");
+
+
+//2. Write a script to display following message on your web
+
+//alert("error! pleas enter a valid password.");
+
+
+
+//3. Write a script to display following message on your web
+//page: (Hint : Use line break)
+
+//alert("Wellcome to JS Land... \ Happy Coding!");
+
+
+
+//4. Write a script to display following messages in sequence:
+//alert("Wellcome to JS Land");
+  //Display the second alert after the first one is closed
+ //alert("You are now entering the world of JavaScript coding!");
+
+
+
+//5. Generate the following message through browser’s
+//developer console:
+//console.log("Hello...I can run JS through my web browser's console");
+
+
+
+
+//6. Make use of alerts in your new/existing HTML & CSS
+//project.
+//alert('You clicked the button!');
+
+
+
+
+//7. Practice placement of <script></script> element in
+//following sections of your project in exercise 6:
+//a. Head
+//b. Body (before your page’s HTML)
+//c. Body (inside your page’s HTML)
+//d. Body (after your page’s HTML)
+//<script src="script.js"></script>
+//<script src="script.js"></script>
+//console.log("This script is placed inside the body.");
+//<script src="script.js"></script>
+
